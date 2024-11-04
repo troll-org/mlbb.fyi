0fa8ce0200985a10d8d309404cd168fe3dfcbcad
raymondtju-locofy <raymond.rtju@gmail.com>
2024-11-05 00:34:37 +0700

    refactor: simplify NotFoundPage button structure by removing unnecessary wrapper

--- app/not-found/page.tsx
+++ app/not-found/page.tsx
@@ -14,17 +14,15 @@ const NotFoundPage = () => {
     <div className="flex h-screen flex-col items-center justify-center">
       <h1 className="font-heading text-4xl">Ooops...</h1>
       <h2 className="mb-4 font-semibold">This page cannot be found</h2>
-      <Button>
-        <Link
-          href="/"
-          className={cn(
-            "w-52 rounded-2xl",
-            buttonVariants({ variant: "gradiantNavySec" })
-          )}
-        >
-          Back to homepage
-        </Link>
-      </Button>
+      <Link
+        href="/"
+        className={cn(
+          buttonVariants({ variant: "gradiantNavySec" }),
+          "w-52 rounded-2xl"
+        )}
+      >
+        Back to homepage
+      </Link>
     </div>
   );
 };
