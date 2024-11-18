0f69d1f595d3675a2f5e72433e3330d05b727aca
raymondtju-locofy <raymond.rtju@gmail.com>
2024-11-18 13:50:56 +0700

    update: change header to paragraph for project disclaimer

--- app/page.tsx
+++ app/page.tsx
@@ -28,12 +28,12 @@ export default async function Home() {
 
           <div className="flex flex-col items-center justify-center pt-[100px]">
             <div className="flex items-end justify-center">
-              <h1 className="text-center text-[12px] text-neutral-500">
+              <p className="text-center text-[12px] text-neutral-500">
                 mlbb.fyi was made as part of 2023 Orbital project, and is not
                 endorsed by Moonton or Mobile Legends: Bang Bang. mlbb.fyi does
                 not reflect the views or opinions of Moonton or anyone
                 officially involved in producing or managing Mobile Legends.
-              </h1>
+              </p>
             </div>
           </div>
         </div>
