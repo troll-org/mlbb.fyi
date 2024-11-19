0ce1e607a4ae36df3bb18f79105bc118c1bd6f92
jayson237 <jng277270@gmail.com>
2024-11-19 17:39:33 +0800

    chore: audit fix package

--- app/wiki/heroes/[hero]/page.tsx
+++ app/wiki/heroes/[hero]/page.tsx
@@ -1,24 +1,8 @@
-import getHeroBuild from "@/lib/actions/getHeroBuild";
-import getHeroSpell from "@/lib/actions/getHeroSpell";
-import getHeroEmblem from "@/lib/actions/getHeroEmblem";
-import getHeroCounter from "@/lib/actions/getHeroCounter";
-import getHeroCorr from "@/lib/actions/getHeroCorr";
-
-import prisma from "@/lib/prismadb";
 import Redirect from "@/components/redirect";
 import getHeroStats from "@/lib/actions/getHeroStats";
 import { getOneHero } from "@/lib/actions/getHeroes";
 import HeroDetails from "@/components/wiki/heroes/hero-details";
 import { getHeroTier } from "@/lib/actions/getHeroTier";
-
-async function findIndexById(arr: any[], targetId: string): Promise<number> {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].id === targetId) {
-      return i;
-    }
-  }
-  return -1;
-}
 
 export default async function HeroPage({
   params,
@@ -34,34 +18,5 @@ export default async function HeroPage({
   const heroStats = await getHeroStats(hero?._id.toString());
   const heroTier = await getHeroTier(hero?._id.toString());
 
-  // const [heroBuild, heroSpell, heroEmblem, heroCounter, heroCorr] =
-  //   await Promise.all([
-  //     getHeroBuild(isExistingHero.id),
-  //     getHeroSpell(isExistingHero.id),
-  //     getHeroEmblem(isExistingHero.id),
-  //     getHeroCounter(isExistingHero.id),
-  //     getHeroCorr(isExistingHero.id),
-  //   ]);
-
-  // const strongAgainst = heroCorr.data?.map((item: any) => item.heroId) || [];
-
-  return (
-    <>
-      <HeroDetails
-        hero={hero}
-        heroStats={heroStats}
-        heroTier={heroTier}
-        // hero={hero}
-        // heroBuild={heroBuild.data?.items || []}
-        // heroSpell={heroSpell.data?.spells || []}
-        // heroEmblem={heroEmblem.data?.emblems || []}
-        // heroWeakAgainst={heroCounter.data?.counters || []}
-        // heroBuild={[]}
-        // heroSpell={[]}
-        // heroEmblem={[]}
-        // heroWeakAgainst={[]}
-        // heroStrongAgainst={strongAgainst}
-      />
-    </>
-  );
+  return <HeroDetails hero={hero} heroStats={heroStats} heroTier={heroTier} />;
 }
