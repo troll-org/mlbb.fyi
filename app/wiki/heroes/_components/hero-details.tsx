1dbf6cd071f44cad52c0655f6cd5da693c340d09
Jayson Ng <110618399+ng-jayson@users.noreply.github.com>
2024-11-22 00:04:22 +0800

    Update hero-details.tsx

--- app/wiki/heroes/_components/hero-details.tsx
+++ app/wiki/heroes/_components/hero-details.tsx
@@ -238,7 +238,7 @@ function HeroDetails({
               alt={heroEquipment.authorName}
               width={40}
               height={40}
-              className="h-[40px] w-[40px] justify-start rounded-full bg-aqua/70 object-cover object-[100%_100%]"
+              className="h-[40px] w-[40px] justify-start rounded-full bg-aqua/70 object-right-bottom"
             />
             <div className="flex flex-col">
               <div className="flex flex-col">
