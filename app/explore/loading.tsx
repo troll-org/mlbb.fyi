2860db7ff1d15e2109fee4b4707b99801d8d825a
ng-jayson <e1075537@u.nus.edu>
2025-02-26 03:40:33 +0800

    test layout

--- app/explore/loading.tsx
+++ app/explore/loading.tsx
@@ -1,6 +1,6 @@
 function Loading() {
   return (
-    <div className="mx-auto mb-72 flex items-center justify-center pt-72">
+    <div className="mx-auto flex items-center justify-center pb-72 pt-72">
       <svg
         fill="none"
         className="h-8 w-8 animate-spin text-white "
