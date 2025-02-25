52403cae7a8f9ce4743772fd27fcfadc2c73fdfc
Jayson Ng <110618399+ng-jayson@users.noreply.github.com>
2025-02-26 00:01:52 +0800

    Update page.tsx

--- app/wiki/page.tsx
+++ app/wiki/page.tsx
@@ -47,7 +47,7 @@ const quickLinks = [
   },
   {
     icon: <TrophyIcon size={ICON_SIZE} />,
-    label: "Tournaments Statistics",
+    label: "Tournament Statistics",
     link: "/wiki/statistics",
   },
   {
@@ -64,16 +64,16 @@ export default async function WikiPage() {
         Mobile Legends Wiki
       </h1>
 
-      <div className="mt-4 grid grid-cols-2 gap-6">
+      <div className="mt-4 grid grid-cols-2 gap-4 lg:gap-5">
         {quickLinks.map((item) => (
           <Link href={item.link} key={item.link}>
-            <GradiantCard className="group relative flex w-[93.3333vw] flex-col justify-start overflow-hidden p-7 md:w-full md:px-6 md:py-5 lg:px-10 lg:py-7">
+            <GradiantCard className="group relative flex w-[93.3333vw] h-[37.57vw] flex-col justify-start overflow-hidden p-7 md:w-full md:h-full md:px-6 md:py-5 lg:px-10 lg:py-7">
               {/* <div className="absolute -left-8 -top-[4vw] h-[24vw] w-[55.556vw] -rotate-[21deg] bg-[#6829A8] opacity-20 blur-2xl md:-left-[20vw] md:-top-[7.5vw] md:h-[11.6vw] md:blur-3xl"></div> */}
-              <div className="absolute bottom-0 left-0 z-10 h-[6vw] w-[100vw] bg-[#2B334A] blur-xl md:h-[2vw] md:w-full"></div>
+              <div className="absolute bottom-0 left-0 z-[-1] h-[6vw] w-[100vw] bg-[#2B334A] blur-xl md:h-[2vw] md:w-full"></div>
 
               {item.icon}
               <div className="mt-4 flex items-center justify-between">
-                <span className="text-lg font-heading lg:text-xl xl:text-2xl">
+                <span className="text-lg font-heading lg:text-xl xl:text-2xl ">
                   {item.label}
                 </span>
 
