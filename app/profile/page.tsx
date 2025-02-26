540c60654c97a7d0a16fe234935df5b155fd0c6a
jayson237 <jng277270@gmail.com>
2025-02-26 19:51:50 +0800

    fix: client exception

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,16 +1,18 @@
+"use server";
+
 import { getCurrentUser } from "@/lib/actions/user";
 import { redirect } from "next/navigation";
 
 const Page = async () => {
   const currentUser = await getCurrentUser();
+
   redirect(
     currentUser
-      ? currentUser.name 
+      ? currentUser.name
         ? `/profile/${currentUser.name}`
         : "/settings"
       : "/auth/signin"
   );
-
 };
 
 export default Page;
